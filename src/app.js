5312fc7d4b791945697ee85e829b9896a4c386f4
Geevan Patel <geevan.peter.patel@gmail.com>
2019-05-23 20:03:07 +0100

    started completion of strings tests

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { sayHello } = require('./lib/strings');
+const { sayHello, uppercase } = require('./lib/strings');
 
 const app = express();
 
 app.get('/strings/hello/:string', (req, res) => {
   res.json({ result: sayHello(req.params.string) });
 });
+
+app.get('strings/upper/:string', (req, res) => {
+  res.json({ result: uppercase(req.params.string) });
+});
+
 
 module.exports = app;
