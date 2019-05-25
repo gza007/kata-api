6e94f48351c7e7effbdc653c4aa57050ab2a89fb
Geevan Patel <geevan.peter.patel@gmail.com>
2019-05-25 23:25:13 +0100

    First 10 Numbers test passing, using isNaN and ParseInt

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { sayHello, uppercase } = require('./lib/strings');
+const { sayHello, uppercase, lowercase, firstCharacter, firstCharacters } = require('./lib/strings');
+const { add } = require('./lib/numbers');
 
 const app = express();
 
@@ -7,9 +8,33 @@ app.get('/strings/hello/:string', (req, res) => {
   res.json({ result: sayHello(req.params.string) });
 });
 
-app.get('strings/upper/:string', (req, res) => {
+app.get('/strings/upper/:string', (req, res) => {
   res.json({ result: uppercase(req.params.string) });
 });
 
+app.get('/strings/lower/:string', (req, res) => {
+  res.json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  if (!req.query.length) {
+    res.json({ result: firstCharacter(req.params.string) });
+  } else {
+    res.json({ result: firstCharacters(req.params.string, req.query.length) });
+  }
+});
+
+app.get('/numbers/add/:a/and/:b', (req, res) => {
+  const isNumeric = (string) => {
+    return !Number.isNaN(parseInt(string));
+  };
+  if (isNumeric(req.params.a) && isNumeric(req.params.b)) {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+    res.status(200).json({ result: add(a, b) });
+  } else {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+});
 
 module.exports = app;
